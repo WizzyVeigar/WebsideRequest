7d1fd25bd03e060e5b4967b882e77e0f602d2aa6
Kenneth Folke Larsen <KennethLarsenBusiness@hotmail.com>
2020-11-10 11:53:24 +0100

    Added a comment

--- WebSideRequestH2/ILogger.cs
+++ WebSideRequestH2/ILogger.cs
@@ -8,6 +8,10 @@ namespace WebSideRequestH2
 {
     interface ILogger
     {
+        /// <summary>
+        /// Generic method for displaying a message
+        /// </summary>
+        /// <param name="message">Message you want displayed</param>
         void LogMessage(string message);
     }
 }
